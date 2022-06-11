5846d20ce9a9e8be7764a29127d9c0672ad2f1dc
Ellian <ellianalba1@gmail.com>
2022-06-11 13:01:27 -0400

    Add the play function

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+"use strict"
 const buttons = document.querySelector('button');
 const playerSelection = document.getElementById('player-selection');
 const computerSelection = document.getElementById('computer-selection');
@@ -14,5 +15,29 @@ const computerPlay = () => {
         computerSelection.innerText = 'paper';
     } else if (choice === 'scissors') {
         computerSelection.innerText = 'scissors';
+    }
+};
+
+const play = (playerSelection, computerSelection) => {
+    if (playerSelection.innerText === computerSelection.innerText) {
+        result.innerText = `It's a Draw! ${playerSelection.innerText} is equal to ${computerSelection.innerText}.`;
+    } 
+    else if (playerSelection.innerText === 'rock' && computerSelection.innerText === 'scissors') {
+        result.innerText = `You Win! ${playerSelection.innerText} beats ${computerSelection.innerText}.`;
+    }
+    else if (playerSelection.innerText === 'rock' && computerSelection.innerText === 'paper') {
+        result.innerText = `You Lose! ${playerSelection.innerText} beats ${computerSelection.innerText}.`;
+    } 
+    else if (playerSelection.innerText === 'paper' && computerSelection.innerText === 'rock') {
+        result.innerText = `You Win! ${playerSelection.innerText} beats ${computerSelection.innerText}.`;
+    } 
+    else if (playerSelection.innerText === 'paper' && computerSelection.innerText === 'scissors') {
+        result.innerText = `You Lose! ${playerSelection.innerText} beats ${computerSelection.innerText}.`;
+    } 
+    else if (playerSelection.innerText === 'scissors' && computerSelection.innerText === 'paper') {
+        result.innerText = `You Win! ${playerSelection.innerText} beats ${computerSelection.innerText}.`;
+    } 
+    else if (playerSelection.innerText === 'scissors' && computerSelection.innerText === 'rock') {
+        result.innerText = `You Lose! ${playerSelection.innerText} beats ${computerSelection.innerText}.`;
     }
 }
